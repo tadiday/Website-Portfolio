6b7dc93ebd1fa95274a68ccad7a91acf2b0642ce
Peter Cao <peterc03@vt.edu>
2025-02-25 19:18:42 -0500

    make scroll better on experience page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,50 +1,54 @@
-import React from 'react';
-import myImage from '../assets/me1.jpg';
-// import { FaRegLightbulb } from "react-icons/fa";
-import  cloud from '../assets/cloud.avif'; // Import the background image
-
+import { useState, useEffect } from 'react';
+// import myImage from '../assets/me1.jpg';
+// import cloud from '../assets/cloud.avif'; // Background image
 
 const About = ({ refProp }) => {
+  // scroll effect to about
+  const [, setScrollPosition] = useState(0);
+  const [opacity, setOpacity] = useState(1); // State for opacity
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+      setOpacity(Math.max(0, 1 - position / 500)); // Adjust divisor for opacity change speed
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <section ref={refProp} className="min-h-[950px] text-black relative px-[10%] pb-20" 
-    style={{
-      backgroundImage: `url(${cloud})`, 
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center'
-    }}
+    <section
+      ref={refProp}
+      className="min-h-[950px] sticky top-0 text-black px-[10%] pb-20 bg-[#fdf0d5]"
+      style={{
+        // backgroundImage: `url(${cloud})`,
+        // backgroundSize: 'cover',
+        // backgroundPosition: 'center'
+        
+      }}
     >
-      <div className='h-[20%] '></div>
-      <div className="font-header text-[20px] bg-red">
-              <div className="font-rale bg-red">An upcoming Software Engineer</div>
-      </div>
-      <div className='flex flex-col w-full'>
-        {/* <div className='pt-[3rem] pb-[3rem] grid grid-cols-25 grid-flow-row-dense'>  */}
-          {/* <h2 className=' text-[30px] text-[#fca311] col-start-11 col-span-10'>An Upcoming Software Engineer</h2>  */}
-          {/* <FaRegLightbulb className='flex justify-center item-center text-[30px] text-[#fca311] '></FaRegLightbulb> */}
-        {/* </div> */}
-        <div className=''>
-        <h1 className="font-rale flex justify-center w-[100%] h-[70%] sm:text-[100px] md:text-[150px] lg:text-[200px] font-thin">PETER CAO</h1>
-          <div id="about" class="absolute grid grid-cols-25 grid-flow-row-dense">
-            <div className="font-title text-[20px] col-start-3 col-span-8">
+      <div className="h-[20%]"></div>
+      <div className="font-header text-[20px] bg-red"></div>
+      <div className="flex flex-col w-full">
+        <div>
+          <h1
+            className="font-rale flex justify-center w-full h-[70%] sm:text-[100px] md:text-[150px] lg:text-[200px] font-thin"
+            style={{ opacity }} // Apply opacity here
+          >
+            PETER CAO
+          </h1>
+          <div id="about" className="absolute grid grid-flow-row-dense grid-cols-25">
+            <div className="font-title text-[20px] col-start-3 col-span-8" style={{ opacity }}>
               <p>38.7439° N, 77.2405° W</p>
               <p>Fairfax, <span className="font-rale font-[300]">V</span>irginia</p>
             </div>
-            <div className='flex justify-end col-start-3 col-span-4 rounded-[20px]'>
-              <div className=''>
-                {/* <img className= "object-contain rounded-[10px]" src={myImage} alt="Peter Cao" /> */}
-              </div>
-              
+            <div className="flex justify-end col-start-3 col-span-4 rounded-[20px]" style={{ opacity }}>
+              <div>{/* <img className="object-contain rounded-[10px]" src={myImage} alt="Peter Cao" /> */}</div>
             </div>
-            
-            <div className='col-[13_/_span_8]  px-10 py-4 rounded-[10px]'>
-              {/* <h2 className='my-[26px] lg:leading-[4rem] sm:text-[20px] md:text-[30px] lg:text-[54px] font-bold'>About Me</h2>
-              <h2 className='my-[26px] lg:leading-[4rem] sm:text-[20px] md:text-[30px] lg:text-[54px] font-bold'> I'm a computer science student at <a className='underline' href='https://www.vt.edu/'> Virginia Tech</a>.</h2>
-              <p className='my-[26px]  sm:text-[12px] md:text-[16px] lg:text-[25px]'> I was a software engineer intern at <a className='underline' href='https://www.vt.edu/'>Card Isle</a> during Fall 2023. I am currently an undergraduate teaching assistant for<a className='underline' href='https://www.vt.edu/'> CS 2506 </a>  (Computer Organization)  
-              and a researcher at <a className='underline' href='https://www.vt.edu/'> IDPro</a>.</p> */}
-              {/* <p className='my-[26px] sm:text-[12px] md:text-[16px] lg:text-[40px] '> My passion for continuous learning is the driving force behind my journey.  */}
-              {/* I thrive on expanding my knowledge and find joy in both sharing my insights
-              with others and learning from the diverse perspectives of my peers.*/}
-              {/* </p>  */}
+            <div className="col-[13_/_span_8] px-10 py-4 rounded-[10px]" style={{ opacity }}>
+              {/* Additional content */}
             </div>
           </div>
         </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -69,8 +69,11 @@ const Experience = ({ refProp }) => {
         role: 'Software Engineer Intern',
         location: 'Blacksburg, VA',
         description: [
-          'Incoming intern Fall 2024.',
-
+          "Contributed to a cyber-threat intelligence platform for detecting attacks, managing malware, and mitigating risks.",
+          "Optimized the CI/CD pipeline by updating Docker Compose configurations, reducing deployment time by 20%",
+          'Diagnosed and resolved Maven build issues and Java test failures, increasing pipeline reliability by 25%.',
+          'Strengthened system performance by addressing over 100+ security vulnerabilities through dependency updates.',
+          'Collaborated using Agile methodologies and GitLab to document and resolve 20+ functionality issues.',
         ],
         image: Peraton
       },
@@ -90,15 +93,15 @@ const Experience = ({ refProp }) => {
     ]
   };
 
-const renderExperiences = (category) => {
-  return experiences[category].map((exp, index, array) => {
-    const isEven = index % 2 === 0;
-    const alignmentClass = isEven ? 'justify-end' : 'justify-start';
+  const renderExperiences = (category) => {
+    return experiences[category].map((exp, index, array) => {
+      const isEven = index % 2 === 0;
+      const alignmentClass = isEven ? 'justify-end' : 'justify-start';
 
-    return (
-      <div key={index} className={`flex w-full  ${alignmentClass}`}>
-        <div className="relative w-full mx-4">
-        <div className="absolute left-1/2 transform -translate-x-1/2 w-1 bg-gray-200 h-full">
+      return (
+        <div key={index} className={`flex w-full  ${alignmentClass}`}>
+          <div className="relative w-full mx-4">
+            {/* <div className="absolute w-1 h-full transform -translate-x-1/2 bg-gray-200 left-1/2">
           <div className="absolute -left-10 transform -translate-y-1/2 w-[85px] h-[85px] bg-white rounded-full flex items-center justify-center">
             <img className='rounded-full w-[80px] h-[80px] object-contain' src={exp.image} alt={exp.image} />
           </div>
@@ -106,44 +109,44 @@ const renderExperiences = (category) => {
             <div className="absolute -left-2 -bottom-4 transform -translate-y-1/2 w-[20px] h-[20px] bg-white rounded-full flex items-center justify-center"> </div>
           )}
         </div>
-          <div className={`flex flex-col w-[45%] min-w-[600px] min-h-[250px] ${isEven ? 'ml-auto pr-[5%]' : 'pl-[5%]'}`}>
-            <div className="bg-white flex rounded-[12px] w-full relative p-4 shadow-lg">
-              <div className='flex flex-col gap-5 text-black w-full'>
-                <div className='grid grid-cols-2 text-[16px] font-bold'>
-                  <h1>{exp.title}</h1>
-                  <h2 className='text-right'>{exp.date}</h2>
-                  <h2>{exp.role}</h2>
-                  <h2 className='text-right'>{exp.location}</h2>
+         */}
+            <div className={`flex flex-col w-[45%] min-w-[600px] min-h-[250px] ${isEven ? 'ml-auto pr-[5%]' : 'pl-[5%]'}`}>
+              <div className="bg-white flex rounded-[12px] w-full relative p-4 shadow-lg">
+                <div className='flex flex-col w-full gap-5 text-black'>
+                  <div className='grid grid-cols-2 text-[16px] font-bold'>
+                    <h1>{exp.title}</h1>
+                    <h2 className='text-right'>{exp.date}</h2>
+                    <h2>{exp.role}</h2>
+                    <h2 className='text-right'>{exp.location}</h2>
+                  </div>
+                  <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
+                    {exp.description.map((desc, i) => <li key={i}>{desc}</li>)}
+                  </ul>
                 </div>
-                <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-                  {exp.description.map((desc, i) => <li key={i}>{desc}</li>)}
-                </ul>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  });
-};
+      );
+    });
+  };
 
 
 
   return (
-    <div id="body-exp" ref={refProp} className="h-full -mt-[20px] min-h-[1000px] bg-black rounded-[20px] justify-center item-center z-10"
-    
-    >
-      {/* <div className='flex justify-center item-center text-[36px]'>Works</div> */}
-      <div className='flex justify-center items-centeuur text-[36px] my-10 pb-10'>
-        <div className='w-[60%]  min-w-[750px] flex bg-[#e0e1dd] rounded-[10px] overflow-hidden text-black font-rale'>
+    <div id="body-exp" ref={refProp} className="h-full -mt-[20px] min-h-[500px] bg-black rounded-[20px] flex flex-col items-center z-10">
+
+      {/* Button Group */}
+      <div className="flex justify-center items-center text-[36px] my-10 pb-10">
+        <div className="w-[60%] min-w-[750px] flex bg-[#e0e1dd] rounded-[10px] overflow-hidden text-black font-rale">
           <button
-            className={`w-[40%] px-10 mx-0 ${activeButton === 'personal' ? 'bg-[#1b263b]  ' : ''}`}
+            className={`w-[40%] px-10 mx-0 ${activeButton === 'personal' ? 'bg-[#1b263b]' : ''}`}
             onClick={() => handleButtonClick('personal')}
           >
             Personal
           </button>
           <button
-            className={`w-[40%] px-10 mx-0 ${activeButton === 'professional' ? 'bbg-[#1b263b]' : ''}`}
+            className={`w-[40%] px-10 mx-0 ${activeButton === 'professional' ? 'bg-[#1b263b]' : ''}`}
             onClick={() => handleButtonClick('professional')}
           >
             Professional
@@ -156,162 +159,92 @@ const renderExperiences = (category) => {
           </button>
         </div>
       </div>
-      <div className=' p-4'>
+
+      {/* Render Experiences */}
+      {/* <div className="p-4">
         {renderExperiences(activeButton)}
+      </div> */}
+
+      {/* Sticky Section */}
+      <div className="relative z-20 w-full bg-secondary-400 overflow-x-clip">
+        <section className="p-[5%] rounded-t-3xl min-h-[2000px]">
+          <div className="w-full pt-space-lg"> {/* Ensure enough scroll space */}
+
+            {/* Sticky Element */}
+            <div className='flex flex-col justify-between mt-12 gap-y-16'>
+              <div class="sticky top-[calc(20vh+0em)] mb-[17.25em]">
+                <div className="border-t border-t-secondary-300">
+
+                  {/* Title */}
+                  <h3 className="font-semibold text-heading-2 text-accent-400 text-[30px]">
+                    Peraton, Aug 2024 - Dec 2024
+                  </h3>
+
+                  {/* Description */}
+                  <ul className="flex flex-col gap-2 pl-4 list-disc ">
+                    <li>Contributed to a cyber-threat intelligence platform for detecting attacks, managing malware, and mitigating risks.</li>
+                    <li>Optimized the CI/CD pipeline by updating Docker Compose configurations, reducing deployment time by 20%.</li>
+                    <li>Diagnosed and resolved Maven build issues and Java test failures, increasing pipeline reliability by 25%.</li>
+                    <li>Strengthened system performance by addressing over 100+ security vulnerabilities through dependency updates.</li>
+                    <li>Collaborated using Agile methodologies and GitLab to document and resolve 20+ functionality issues.</li>
+                  </ul>
+
+
+                </div>
+              </div>
+              <div className="sticky top-[calc(20vh+5.75em)] mb-[11.25em] bg-black">
+                <div className="border-t border-t-secondary-300">
+
+                  {/* Title */}
+                  <h3 className="font-semibold text-heading-2 text-accent-400 text-[30px]">
+                    Card Isle, Aug 2024 - Dec 2024
+                  </h3>
+
+                  {/* Description */}
+                  <ul className="flex flex-col gap-2 pl-4 list-disc">
+                    <li>experiences.</li>
+                    <li>Optimized the CI/CD pipeline by updating Docker Compose configurations, reducing deployment time by 20%.</li>
+                    <li>Diagnosed and resolved Maven build issues and Java test failures, increasing pipeline reliability by 25%.</li>
+                    <li>Strengthened system performance by addressing over 100+ security vulnerabilities through dependency updates.</li>
+                    <li>Collaborated using Agile methodologies and GitLab to document and resolve 20+ functionality issues.</li>
+                  </ul>
+
+
+                </div>
+              </div>
+              <div className="sticky top-[calc(20vh+11.5em)] mb-[5.75em] bg-secondary-400 p-5 shadow-lg z-[50] bg-black">
+                <div className="border-t border-t-secondary-300">
+
+                  {/* Title */}
+                  <h3 className="font-semibold text-heading-2 text-accent-400">
+                    Card Isle, Aug 2024 - Dec 2024
+                  </h3>
+
+                  {/* Description */}
+                  <ul className="flex flex-col gap-2 list-disc pl-4 text-[14px]">
+                    <li>experiences.</li>
+                    <li>Optimized the CI/CD pipeline by updating Docker Compose configurations, reducing deployment time by 20%.</li>
+                    <li>Diagnosed and resolved Maven build issues and Java test failures, increasing pipeline reliability by 25%.</li>
+                    <li>Strengthened system performance by addressing over 100+ security vulnerabilities through dependency updates.</li>
+                    <li>Collaborated using Agile methodologies and GitLab to document and resolve 20+ functionality issues.</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </section>
+
+        <section className="p-[5%] rounded-t-3xl min-h-[2000px]">
+
+
+        </section>
       </div>
 
-      <div className='flex justify-center items-center flex-col gap-5 p-4 '>
-      {/* --------------------------------Personal Experience ------------------------------------ */}
-        {/* {activeButton === 'personal' && (
-
-          <div id = "academic" className='bg-white flex rounded-[12px] w-[40%]'>
-          <img className='w-[32%] rounded-[12px] object-contain' src={VT} alt='Card Isle' />
-            <div className='p-4 flex flex-col gap-5 text-black'>
-              <div className='grid grid-cols-2 text-[16px]'>
-                <h1 className=''>Virginia Tech Computer Science</h1>
-                <h2 className='text-right'>May 2023 - Present</h2>
-                <h2 className=''>Undergraduate Teaching Assistant</h2>
-                <h2 className='text-right'>Blacksburg, VA</h2>
-              </div>
-              <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-                <li> Courses: CS 2505 & CS 2506 - Computer Organization I & II.</li>
-                <li> Conducted office hours, providing troubleshooting support and answering questions to enhance students' understanding.</li>
-                <li> Actively guided and monitored online forums and lab sessions to provide interactive learning experiences. </li>
-              </ul>
-            </div>
-          </div>
-        )}
-        
-        {activeButton === 'personal' && (
-        <div id = "academic" className='bg-white flex rounded-[12px] w-[40%]'>
-          <img className='w-[32%] rounded-[12px] object-contain' src={VT} alt='Card Isle' />
-          <div className='p-4 flex flex-col gap-5 text-black'>
-            <div className='grid grid-cols-2 text-[16px]'>
-              <h1 className=''>Virginia Tech IDPro</h1>
-              <h2 className='text-right'>Jan 2024 - May 2024</h2>
-              <h2 className=''>Rural Trash Collection Research</h2>
-              <h2 className='text-right'>Blacksburg, VA</h2>
-            </div>
-            <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-              <li> Developing an automated trash collection robot, allowing the monitor/operator to work remotely.</li>
-              <li> Selecting and installing sensors necessary for autonomous navigation and trash collection.</li>
-              <li> Identifying and reviewing algorithms to govern path planning, obstacle avoidance, safety, and trash collection. </li>
-            </ul>
-          </div>
-        </div>
-        )}
-
-      {activeButton === 'personal' && (
-        <div id = "academic" className='bg-white flex rounded-[12px] w-[40%]'>
-          <img className='w-[32%] rounded-[12px] object-contain' src={VT} alt='Card Isle' />
-          <div className='p-4 flex flex-col gap-5 text-black'>
-            <div className='grid grid-cols-2 text-[16px]'>
-              <h1 className=''>Virginia Tech IDPro</h1>
-              <h2 className='text-right'>Sep 2023 - Nov 2023</h2>
-              <h2 className=''>SMART Research</h2>
-              <h2 className='text-right'>Blacksburg, VA</h2>
-            </div>
-            <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-              <li> Conducted research and successfully designed a Software Managed Arduino-based Residential Toolkit (SMART).</li>
-              <li> Integrated Raspberry Pi to facilitate connectivity and optimized system functionality for an enhanced user experience.</li>
-              <li> Engineered a user-friendly mobile application using Swift, enabling remote control and monitoring of smart devices.</li>
-            </ul>
-          </div>
-        </div>
-      )} */}
-
-    
-      {/* --------------------------------Academic Experience ------------------------------------ */}
-        {/* {activeButton === 'academic' && (
-          
-        <div className='w-full flex justify-center'>
-        <div className='leading-3  '>
-
-        </div>
-
-        <div id = "left "className="bg-white flex rounded-[12px] p-4  w-[45%]  min-w-[700px] shadow-lg">
-          <img className='w-[32%] rounded-[12px]' src={VT} alt='Card Isle' />
-            <div className='p-4 flex flex-col gap-5 text-black'>
-              <div className='grid grid-cols-2 text-[16px]'>
-                <h1 className=''>Virginia Tech Computer Science</h1>
-                <h2 className='text-right'>May 2023 - Present</h2>
-                <h2 className=''>Undergraduate Teaching Assistant</h2>
-                <h2 className='text-right'>Blacksburg, VA</h2>
-              </div>
-              <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-                <li> Courses: CS 2505 & CS 2506 - Computer Organization I & II.</li>
-                <li> Conducted office hours, providing troubleshooting support and answering questions to enhance students' understanding.</li>
-                <li> Actively guided and monitored online forums and lab sessions to provide interactive learning experiences. </li>
-              </ul>
-            </div>
-          </div>
-          </div>
-            
-            
-        )}
-        
-        {activeButton === 'academic' && (
-        <div className="bg-white flex rounded-[12px] p-4  w-[45%]  min-w-[700px] shadow-lg">
-        <img className='w-[32%] rounded-[12px]' src={VT} alt='Card Isle' />
-          <div className='p-4 flex flex-col gap-5 text-black'>
-            <div className='grid grid-cols-2 text-[16px]'>
-              <h1 className=''>Virginia Tech IDPro</h1>
-              <h2 className='text-right'>Jan 2024 - May 2024</h2>
-              <h2 className=''>Rural Trash Collection Research</h2>
-              <h2 className='text-right'>Blacksburg, VA</h2>
-            </div>
-            <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-              <li> Developing an automated trash collection robot, allowing the monitor/operator to work remotely.</li>
-              <li> Selecting and installing sensors necessary for autonomous navigation and trash collection.</li>
-              <li> Identifying and reviewing algorithms to govern path planning, obstacle avoidance, safety, and trash collection. </li>
-            </ul>
-          </div>
-        </div>
-        )}
-
-      {activeButton === 'academic' && (
-        <div className="bg-white flex rounded-[12px] p-4  w-[45%]  min-w-[700px] shadow-lg">
-        <img className='w-[32%] rounded-[12px]' src={VT} alt='Card Isle' />
-          <div className='p-4 flex flex-col gap-5 text-black'>
-            <div className='grid grid-cols-2 text-[16px]'>
-              <h1 className=''>Virginia Tech IDPro</h1>
-              <h2 className='text-right'>Sep 2023 - Nov 2023</h2>
-              <h2 className=''>SMART Research</h2>
-              <h2 className='text-right'>Blacksburg, VA</h2>
-            </div>
-            <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-              <li> Conducted research and successfully designed a Software Managed Arduino-based Residential Toolkit (SMART).</li>
-              <li> Integrated Raspberry Pi to facilitate connectivity and optimized system functionality for an enhanced user experience.</li>
-              <li> Engineered a user-friendly mobile application using Swift, enabling remote control and monitoring of smart devices.</li>
-            </ul>
-          </div>
-        </div>
-      )}
-
-
-
-        {/* --------------------------------Professional Experience ------------------------------------ */}
-        {/* {activeButton === 'professional' && (
-          <div id="professional" className='bg-white flex rounded-[12px] w-[40%]'>
-            <img className='w-[32%] rounded-[12px] object-contain' src={cardIsle} alt='Card Isle' />
-            <div className='p-4 flex flex-col gap-5 text-black'>
-              <div className='grid grid-cols-2 text-[16px]'>
-                <h1 className=''>Card Isle</h1>
-                <h2 className='text-right'>Sep 2023 - Nov 2023</h2>
-                <h2 className=''> Software Engineer Intern</h2>
-                <h2 className='text-right'>Blacksburg, VA</h2>
-              </div>
-              <ul className='flex flex-col gap-2 list-disc pl-4 text-[14px]'>
-                <li> Collaborated as a front-end development and testing intern, actively contributing to client projects.</li>
-                <li> Utilized Alpine.js to craft dynamic and responsive web interfaces, enhancing user interactions and experiences.</li>
-                <li> Implemented Playwright testing for a demo website to ensure functionality within a single-page layout. </li>
-              </ul>
-            </div>
-          </div>
-        )} */}
-
-      </div>
     </div>
+
+
   );
 };
 
