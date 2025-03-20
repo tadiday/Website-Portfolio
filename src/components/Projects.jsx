31548cadf3dc0a3260943dc419525f41c0e82c63
Peter Cao <peterc03@vt.edu>
2025-03-20 17:18:45 -0400

    finished another layout for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import cardIsle from '../assets/cardisle.png';
-import VirginiaTech from '../assets/VT.jpg';
-import WebPortfolio from '../assets/web-port.png';
+import WebPortfolio from '../assets/web-port.gif';
 import SimPliSplit from '../assets/simplisplit.webp';
 import FoodSwipe from '../assets/foodSwipe.png';
 import Background2 from '../assets/background2.avif';
@@ -13,80 +11,59 @@ import NoiseTexture from '../assets/noise.jpg';
 
 const Project = ({ refProp }) => {
 
+  const projects = [
+    {
+      title: 'Web-Portfolio',
+      date: '2025',
+      tech: ['React.js', 'Tailwind', 'Next.js'],
+      gitHub: 'link',
+      description: `
+          Built a personal portfolio website using React.js and Tailwind CSS to showcase projects, experiences, and research.`,
+      image: WebPortfolio,
+      tags: ['Web-App', 'GitHub', '2025'],
+      backg: Background3,
+    },
+    {
+      title: 'Simplisplit',
+      date: '2023',
+      tech: ['React-Native', 'Tailwind CSS', 'Python', 'Flask'],
+      gitHub: 'link',
+      description: `
+          A mobile application that allows user to scan their bills and split the cost with their friends seamlessly. 
+          Available through the App Store.`,
+      image: SimPliSplitGif,
+      tags: ['Mobile', 'GitHub', '2023'],
+      backg: Background1,
+    },
+    {
+      title: 'Food Swipe',
+      date: '2022',
+      tech: ['React.js', 'TypeScript', 'Tailwind', 'Next.js'],
+      gitHub: 'link',
+      description: `
+          Developed a Tinder-inspired restaurant discovery app designed to help users find the perfect dining spot. 
+          The app allows users to swipe right for more details on local restaurants tailored to their location.`,
+      image: FoodSwipe,
+      tags: ['Web-App', 'GitHub', '2022'],
+      backg: Background2,
+    },
+  ]
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const experiences = {
-    projects: [
-      {
-        title: 'Food Swipe hi',
-        date: 'Nov 2022',
-        tech: ['React-Native', 'Tailwind CSS'],
-        gitHub: 'link',
-        description: `
-          Developed a seamless group payment solution using React Native, 
-          integrating the Veryfi API and Flask for accurate data extraction 
-          from receipts. The app streamlines bill splitting by leveraging 
-          the Venmo API for payment requests, reducing manual calculations 
-          and improving user convenience.`,
-        description2: [
-          'Courses: CS 2505 & CS 2506 - Computer Organization I & II.',
-          'Conducted office hours, providing troubleshooting support and answering questions to enhance students\' understanding.',
-          'Actively guided and monitored online forums and lab sessions to provide interactive learning experiences.'
-        ],
-        image: FoodSwipe
-      },
-      {
-        title: 'Simplisplit',
-        date: 'Sep 2023',
-        tech: ['React-Native', 'Tailwind CSS', 'Python', 'Flask'],
-        gitHub: 'link',
-        description: `
-          Developed a seamless group payment solution using React Native, 
-          integrating the Veryfi API and Flask for accurate data extraction 
-          from receipts. The app streamlines bill splitting by leveraging 
-          the Venmo API for payment requests, reducing manual calculations 
-          and improving user convenience.`,
-        description2: [
-          'Courses: CS 2505 & CS 2506 - Computer Organization I & II.',
-          'Conducted office hours, providing troubleshooting support and answering questions to enhance students\' understanding.',
-          'Actively guided and monitored online forums and lab sessions to provide interactive learning experiences.'
-        ],
-        image: SimPliSplit
-      },
-      {
-        title: 'Web-Portfolio',
-        date: 'May 2025',
-        tech: ['React-Native', 'Tailwind CSS', 'Next.js'],
-        gitHub: 'link',
-        description: `
-          Developed a seamless group payment solution using React Native, 
-          integrating the Veryfi API and Flask for accurate data extraction 
-          from receipts. The app streamlines bill splitting by leveraging 
-          the Venmo API for payment requests, reducing manual calculations 
-          and improving user convenience.`,
-        description2: [
-          'Courses: CS 2505 & CS 2506 - Computer Organization I & II.',
-          'Conducted office hours, providing troubleshooting support and answering questions to enhance students\' understanding.',
-          'Actively guided and monitored online forums and lab sessions to provide interactive learning experiences.'
-        ],
-        image: WebPortfolio
-      }
-    ],
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
   };
-  const [, setScrollPosition] = useState(0);
-  const [opacity, setOpacity] = useState(1); // State for opacity
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.pageYOffset;
-      let num = position * 0.2
-      console.log(position)
-      setScrollPosition(position);
-      setOpacity(Math.max(0, 1 - position / 2000)); // Adjust divisor for opacity change speed
-    };
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  const { title, description, tech, image, tags } = projects[currentIndex];
+  console.log(tech)
+
+
 
   return (
     <div ref={refProp} className="h-full rounded-t-[25px] bg-[#080807] p-4 z-30">
@@ -98,6 +75,103 @@ const Project = ({ refProp }) => {
             </h2>
           </div>
         </div>
+
+        <section className="pr-[5%] pb-[5%] pl-[5%] rounded-t-3xl min-h-[2000px]">
+          <div className="w-full pt-16">
+            <div className="flex flex-col justify-between mt-12 gap-y-16">
+              {/* Portfolio */}
+              <div className="sticky top-0 border-t border-[#524D47] bg-[#080807] pb-[10em] " style={{ top: 'calc(10vh + 0em)' }}>
+
+                <div className="grid gap-x-8 relative h-full min-h-[30vh] flex-col place-items-start pt-1 md:grid md:min-h-[40vh] md:grid-cols-12">
+                  <h3 className="col-span-5 col-start-1 font-semibold py-4 2xl:py-3 text-[3.5rem]">{title}</h3>
+                  <div class="flex items-center gap-x-[10px] col-span-5 col-start-6 h-full text-[20px] ">
+                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">Web-App</span>
+                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">GitHub</span>
+                    <span class="border rounded-full border-[#443e37] flex px-3 py-1 bg-[#080807]">2025</span>
+                  </div>
+                  <div className="flex flex-col w-full h-full col-span-5 col-start-1 text-heading">
+                    <div className="flex flex-col w-full h-full col-span-5 col-start-1 pt-4 gap-y-4 text-heading ">
+                      <p className="font-thin text-balance text-[1.25rem] ">
+                        {description}
+                      </p>
+
+                      <div className="pt-4 border-t border-[#524D47] relative ">
+                        <div className='flex flex-col items-start gap-x-6'>
+                          <div className="flex items-end gap-x-[10px] tracking-base">
+
+                            {tech.map((item, index) => (
+                              <span className="border rounded-full border-[#524D47] flex px-3 py-1" key={index}>
+                                {item}
+                              </span>
+                            ))}
+                          </div>
+                        </div>
+
+                        {/* Buttons Positioned Bottom-Right */}
+
+                      </div>
+                    </div>
+                    <div className=''>
+                      <div className='flex flex-col items-start h-full gap-x-6'>
+                        <div className="flex items-end gap-x-[10px] bottom-0 ">
+                          <button
+                            onClick={handlePrevious}
+                            className="border rounded-full border-[#524D47] text-[18px] flex px-7 py-1 bg-[#080807]"
+                          >
+                            Prev
+                          </button>
+                          <button
+                            onClick={handleNext}
+                            className="border rounded-full border-[#524D47] text-[18px] flex px-7 py-1 bg-[#080807]"
+                          >
+                            Next
+                          </button>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+
+
+
+
+
+
+
+
+
+                  <div className='relative top-0 flex items-center justify-center w-full col-span-7 col-start-6 marker:rounded-md aspect-square overflow-clip max-h-[600px]'>
+                    <img className="absolute object-cover w-full h-full aspect-4/3 rounded-xl brightness-75 contrast-125"
+                      src={Background3}
+                      alt="background2" >
+                    </img>
+
+
+                    <div
+                      className={`z-10 w-5/6 space-y-4 rounded-lg aspect-4/3 overflow-clip 
+                        ${title === 'Simplisplit' ? 'border-[#524D47] shadow-xl w-[30%] rounded-xl object-fit' : ''}`}
+                    >
+                      <img className="border-gray-700 rounded-lg shadow-md"
+                        src={image}
+                        alt={title} >
+                      </img>
+                    </div>
+
+                  </div>
+
+
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+
+
+
+
         <section className="pr-[5%] pb-[5%] pl-[5%] rounded-t-3xl min-h-[2000px]">
           <div className="w-full pt-16">
             <div className="flex flex-col justify-between mt-12 gap-y-16">
@@ -189,6 +263,7 @@ const Project = ({ refProp }) => {
                     <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">GitHub</span>
                     <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">2023</span>
                   </div>
+
                 </div>
               </div>
 
@@ -197,6 +272,11 @@ const Project = ({ refProp }) => {
 
                 <div className="grid gap-x-8 relative font-semibold min-h-[30vh] flex-col place-items-start pt-1 md:grid md:min-h-[40vh] md:grid-cols-12">
                   <h3 className="col-span-5 col-start-1 py-4 2xl:py-3 text-[3.5rem]">FoodSwipe</h3>
+                  <div class="flex items-center gap-x-[10px] col-span-5 col-start-6 h-full text-[20px]">
+                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">Web-App</span>
+                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">GitHub</span>
+                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">2022</span>
+                  </div>
                   <div className="flex flex-col w-full col-span-5 col-start-1 pt-4 gap-y-4 text-heading">
                     <p className="font-thin text-balance text-[1.25rem] ">
                       Developed a Tinder-inspired restaurant discovery app designed to help users find the perfect dining spot.
@@ -231,10 +311,20 @@ const Project = ({ refProp }) => {
                     </div>
 
                   </div>
-                  <div class="flex items-end gap-x-[10px] col-span-5 col-start-6 pt-5">
-                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">Web-App</span>
-                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">GitHub</span>
-                    <span class="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]">2022</span>
+
+                  <div className="flex items-end gap-x-[10px] col-span-5 col-start-6 pt-5">
+                    <button
+                      onClick={handlePrevious}
+                      className="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]"
+                    >
+                      Prev
+                    </button>
+                    <button
+                      onClick={handleNext}
+                      className="border rounded-full border-[#524D47] flex px-3 py-1 bg-[#080807]"
+                    >
+                      Next
+                    </button>
                   </div>
                 </div>
               </div>
@@ -266,3 +356,6 @@ const Project = ({ refProp }) => {
 };
 
 export default Project;
+
+
+
