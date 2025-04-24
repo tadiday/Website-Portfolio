9cbfcdf8e6dd86a27495770a5c69b8deed5133e2
Peter Cao <peterc03@vt.edu>
2025-04-24 16:00:59 -0400

    update quotes, profile pic, and navbar layout

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -10,28 +10,28 @@ const NavBar = () => {
   return (
     <nav className="z-10 flex items-center justify-center w-full shadow-lg sm:rounded-tr-2xl sm:rounded-br-2xl bg-bar ">
       <div className="flex flex-row sm:flex-col items-center my-4">
-        <ul className="text-[20px] flex flex-row sm:flex-col gap-y-6 items-center font-thin text-black">
-          <li className='py-[5px] px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
+        <ul className="text-[20px] w-[100%] flex flex-row sm:flex-col gap-y-6 items-center font-thin text-black">
+          <li className='py-[5px] px-7 sm:px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
             <Link href="#home" aria-label="Go to Home">
               <RiHome2Line />
             </Link>
           </li>
-          <li className='py-[5px] px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
+          <li className='py-[5px] px-7 sm:px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
             <Link href="#about" aria-label="Go to About">
               <IoPersonOutline />
             </Link>
           </li>
-          <li className='py-[5px] px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
+          <li className='py-[5px] px-7 sm:px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
             <Link href="#projects" aria-label="Go to Project">
               <IoCodeWorkingSharp />
             </Link>
           </li>
-          <li className='py-[5px] px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
+          <li className='py-[5px] px-7 sm:px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
             <Link href="#experience" aria-label="Go to Experience">
               <MdWorkOutline />
             </Link>
           </li>
-          <li className='py-[5px] px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
+          <li className='py-[5px] px-7 sm:px-4 hover:rounded-full text-symbol hover:bg-[#847A6F]'>
             <Link href="#contact" aria-label="Go to Contact">
               <MdOutlineMailOutline />
             </Link>
